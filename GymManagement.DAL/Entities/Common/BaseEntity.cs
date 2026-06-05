31615390365e638f97f830555944afe4d5e3486a
Mohammed Abed <mbd411071@gmail.com>
2026-06-05 05:27:55 +0300

    feat: add BaseEntity

---
+++ GymManagement.DAL/Entities/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+
+namespace GymManagement.DAL.Entities.Common
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? DeletedAt { get; set; }
+        public int? DeletedBy { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
